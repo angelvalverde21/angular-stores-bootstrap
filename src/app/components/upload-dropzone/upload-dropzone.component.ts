049ea46366eeb1eb0f541b2d1aaf3c841742eb22
angelvalverde21 <angelvalverde21@gmail.com>
2024-08-29 11:33:57 -0500

    se agrego la pagina de config

--- src/app/components/upload-dropzone/upload-dropzone.component.ts
+++ src/app/components/upload-dropzone/upload-dropzone.component.ts
@@ -9,37 +9,33 @@ import Dropzone from 'dropzone';
   styleUrl: './upload-dropzone.component.css',
 })
 export class UploadDropzoneComponent {
-
-  dropzoneConfig = {
-    url: '/upload', // Actualiza esto con la URL de tu servidor
-    headers: {
-      'X-CSRF-TOKEN': 'tu-csrf-token', // Actualiza esto según corresponda
-    },
-    dictDefaultMessage: `<div>Sube tus archivos aquí</div> <i class="fas fa-camera mt-5" style="font-size: 18pt;"></i>`,
-    acceptedFiles: 'image/*',
-    paramName: 'file',
-    maxFilesize: 10, // Tamaño máximo en MB
-    init: function () {
-      this.on('complete', function (file) {
-        this.removeFile(file);
-      });
-      this.on('queuecomplete', function () {
-        console.log('Todos los archivos han sido subidos');
-      });
-      this.on('accept', function (file, done) {
-        if (file.name === 'justinbieber.jpg') {
-          done('No puedes subir este archivo.');
-        } else {
-          done();
-        }
-      });
-    },
-  };
-
   ngOnInit(): void {
+    Dropzone.autoDiscover = false; // Desactivar la auto-detección de Dropzone
+
     const dropzone = new Dropzone('#my-dropzone', {
-      url: '/upload', // Configura la URL de subida
-      // Otras configuraciones de Dropzone
+      url: '/upload', // Actualiza esto con la URL de tu servidor
+      headers: {
+        'X-CSRF-TOKEN': 'tu-csrf-token', // Actualiza esto según corresponda
+      },
+      dictDefaultMessage: `<div>Sube tus archivos aquí</div> <i class="fas fa-camera" style="font-size: 18pt;"></i>`,
+      acceptedFiles: 'image/*',
+      paramName: 'file',
+      maxFilesize: 10, // Tamaño máximo en MB
+      init: function () {
+        this.on('complete', (file) => {
+          this.removeFile(file);
+        });
+        this.on('queuecomplete', function () {
+          console.log('Todos los archivos han sido subidos');
+        });
+        this.on('accept', function (file, done) {
+          if (file.name === 'justinbieber.jpg') {
+            done('No puedes subir este archivo.');
+          } else {
+            done();
+          }
+        });
+      },
     });
   }
 }
