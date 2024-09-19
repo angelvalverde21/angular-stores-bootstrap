ff0e836ba5e86111e5e0857c1acb8a2034f09025
angelvalverde21 <angelvalverde21@gmail.com>
2024-09-19 11:12:30 -0500

    se esta ordeando los botones en productPage

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
-
+  @Input() id: string = ""; 
+  @Input() title: string = "Titulo"; 
+  
 }
