e547f654f1ce42c51cbe3be5b20438be7b98cbb5
angelvalverde21 <angelvalverde21@gmail.com>
2024-11-15 20:33:50 -0500

    se agrego nuevo formato

--- src/app/pages/warehouses/warehouse-order-index-page/table-order-index/table-order-index.component.ts
+++ src/app/pages/warehouses/warehouse-order-index-page/table-order-index/table-order-index.component.ts
@@ -1,38 +1,70 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PipesModule } from '../../../../shared/pipes.module';
-import { CardAddressComponent } from "../../../../auth/shared/order/card-address/card-address.component";
 import { OrderService } from '../../../../services/order.service';
 import { StoreService } from '../../../../services/store.service';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { PaymentComponent } from '../../../../components/Order/payment/payment.component';
 
 @Component({
   selector: 'app-table-order-index',
   standalone: true,
-  imports: [RouterModule, CommonModule, PipesModule, CardAddressComponent, RouterModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    PipesModule,
+    RouterModule,
+    NgbDropdownModule,
+    PaymentComponent,
+  ],
   templateUrl: './table-order-index.component.html',
-  styleUrl: './table-order-index.component.css'
+  styleUrl: './table-order-index.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class TableOrderIndexComponent {
-
-  @Input() orders: any; 
-  @Input() store: string = ""; 
-  @Input() warehouse_id: number = 0; 
-  
-  constructor(    
+  @Input() orders: any;
+  @Input() store: string = '';
+  @Input() warehouse_id: number = 0;
+  selectedOrderId: number = 0;
+  constructor(
     private _order: OrderService,
-    private _store: StoreService
-  ){
-    
+    private _store: StoreService,
+    config: NgbModalConfig,
+    private modalService: NgbModal
+  ) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
-
-  status(order:any){
+  status(order: any) {
     return this._order.status(order);
   }
 
-  warehouseName(){
+  warehouseName() {
     const warehouses = this._store.warehouses();
-    return warehouses.find((warehouse:any) => warehouse.id == this.warehouse_id).name;
+    return warehouses.find(
+      (warehouse: any) => warehouse.id == this.warehouse_id
+    ).name;
+  }
+
+  modal: any;
+
+  // uploadComprobanteEnvio(content: TemplateRef<any>) {
+  //   this.modal = this.modalService.open(content, { centered: true });
+  // }
+
+  openModal(content: TemplateRef<any>, orderId: number): void {
+    this.selectedOrderId = orderId;
+    this.modal = this.modalService.open(content, { centered: true });
   }
 
 
